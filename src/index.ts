c6e8e3996d9920ea0c6b6a327de6c1971165af63
Andrei <soprachev@mail.ru>
2024-11-19 03:17:38 +0300

    protobuf

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,61 @@ import { z } from 'zod'
 import { connect, clickhouse } from './db.ts'
 import { insert } from "./batchInsert.ts";
 
+import { loki } from './generated/loki.ts'
+import { uncompress } from 'snappyjs'
+
 const app = new Hono();
 app.use(cors());
 
+const targetTable = `${Bun.env.CLICKHOUSE_DATABASE ?? 'Logs'}.Loki`;
 
 app.post('/loki/api/v1/push',
-  zValidator('json', z.object({
-    streams: z.array(z.object({
-      stream: z.record(z.string()),
-      values: z.array(
-        z.union([
-          z.tuple([
-            z.string(),
-            z.string(),
-            z.record(z.any())
-          ]),
-          z.tuple([z.string(), z.string()])
-        ])
-      ),
-    })),
-  }),
+  async (c, next) => {
+    const contentType = c.req.header('content-type');
+    if (contentType === 'application/json') return await next();
+    if (contentType !== 'application/x-protobuf') return c.text('Invalid content-type', 400);
+
+    try {
+      const buffer = new Uint8Array(await c.req.arrayBuffer());
+      const uncompressed = uncompress(buffer);
+      const message = loki.PushRequest.deserialize(uncompressed);
+
+      insert(targetTable, ...message.streams.flatMap(stream => {
+        const labels = Object.fromEntries([...stream.labels.matchAll(/(\w+)=["']?([^,"'}]+)["']?/g)].map(m => [m[1], m[2]]));
+
+        return stream.entries.map(entry => ({
+          time: `${entry.timestamp.seconds}${entry.timestamp.nanos.toString().padStart(9, '0')}`,
+          message: entry.line,
+          metadata: {
+            ...labels,
+            ...Object.fromEntries(entry.structuredMetadata.map(m => [m.name, m.value]))
+          }
+        }))
+      }));
+
+    } catch (error) {
+      console.error(error);
+      return c.text('Invalid request: \n' + error, 400);
+    }
+
+    return c.text('OK');
+  },
+  zValidator('json',
+    z.object({
+      streams: z.array(z.object({
+        stream: z.record(z.string()),
+        values: z.array(
+          z.union([
+            z.tuple([
+              z.string(),
+              z.string(),
+              z.record(z.any())
+            ]),
+            z.tuple([z.string(), z.string()])
+          ])
+        ),
+      })),
+    }),
     async (result, c) => {
 
       if (!result.success) {
@@ -34,7 +69,7 @@ app.post('/loki/api/v1/push',
 
       const { streams } = result.data;
 
-      insert('Loki.Logs', ...streams.flatMap(stream =>
+      insert(targetTable, ...streams.flatMap(stream =>
         stream.values.map(v => ({
           time: v[0],
           message: v[1],
@@ -53,10 +88,10 @@ if (!await connect({ timeout: 10 })) {
   throw new Error('ClickHouse is not available')
 }
 
-await clickhouse.exec({ query: `create database if not exists Loki` })
+await clickhouse.exec({ query: `create database if not exists ${Bun.env.CLICKHOUSE_DATABASE ?? 'Logs'}` })
 await clickhouse.exec({
   query: `
-create table if not exists Loki.Logs (
+create table if not exists ${targetTable} (
     time DateTime64(9),
     message String,
     metadata JSON,
